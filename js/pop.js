061a30b185dae103fa7482d6c586dcfc6708e2fe
Alex <bab.ivanov@yandex.ru>
2020-03-17 14:05:44 +0300

    texe

---
+++ js/pop.js
@@ -0,0 +1,59 @@
+
+  var link = document.querySelector(".button-say");
+
+  var popup = document.querySelector(".modal-login");
+  var close = popup.querySelector(".modal-close");
+
+  var form = popup.querySelector("form");
+  var login = popup.querySelector("[name=first-name]");
+  var password = popup.querySelector("[name=email]");
+
+  var isStorageSupport = true;
+  var storage = "";
+
+  try {
+    storage = localStorage.getItem("login");
+  } catch (err) {
+    isStorageSupport = false;
+  }
+
+  link.addEventListener("click", function (evt) {
+    evt.preventDefault();
+    popup.classList.add("modal-show");
+
+    if (storage) {
+      login.value = storage;
+      password.focus();
+    } else {
+      login.focus();
+    }
+  });
+
+  close.addEventListener("click", function (evt) {
+    evt.preventDefault();
+    popup.classList.remove("modal-show");
+    popup.classList.remove("modal-error");
+  });
+
+  form.addEventListener("submit", function (evt) {
+    if (!first-name.value || !email.value) {
+      evt.preventDefault();
+      popup.classList.remove("modal-error");
+      popup.offsetWidth = popup.offsetWidth;
+      popup.classList.add("modal-error");
+    } else {
+      if (isStorageSupport) {
+        localStorage.setItem("first-name", first-name.value);
+      }
+    }
+  });
+
+  window.addEventListener("keydown", function (evt) {
+    if (evt.keyCode === 27) {
+      evt.preventDefault();
+      if (popup.classList.contains("modal-show")) {
+        popup.classList.remove("modal-show");
+        popup.classList.remove("modal-error");
+      }
+    }
+  });
